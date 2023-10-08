473a2a2537f1b7e14e19c163c0997bae75f985b9
Kavya Kumar <116029008+kavyak1604@users.noreply.github.com>
2023-10-08 22:53:51 +0530

    Add files via upload

---
+++ kkfunctions.cpp
@@ -0,0 +1,151 @@
+#include <iostream>
+using namespace std;
+int sum(int n,int m)
+{return n+m;}
+double sum(double n,double m)
+{return n+m;}
+double sum(double n,int m)
+{return n+m;}
+double sum(int n,double m)
+{return n+m;}
+
+int sum(int a,int b,int c)
+{return a+b+c;}
+double sum(int a , int b,double c)
+{return a+b+c;}
+double sum(int a , double b,int c)
+{return a+b+c;}
+double sum(int a , double b,double c)
+{return a+b+c;}
+double sum(double a,int b,int c)
+{return a+b+c;}
+double sum(double a,int b,double c)
+{return a+b+c;}
+double sum(double a,double b,int c)
+{return a+b+c;}
+double sum(double a,double b,double c)
+{return a+b+c;}
+int sum(int a, int b, int c, int d) {
+  return a + b + c + d;
+}
+
+double sum(int a, int b, int c, double d) {
+  return a + b + c + d;
+}
+
+double sum(int a, int b, double c, int d) {
+  return a + b + c + d;
+}
+
+double sum(int a, int b, double c, double d) {
+  return a + b + c + d;
+}
+
+double sum(int a, double b, int c, int d) {
+  return a + b + c + d;
+}
+
+double sum(int a, double b, int c, double d) {
+  return a + b + c + d;
+}
+
+double sum(int a, double b, double c, int d) {
+  return a + b + c + d;
+}
+
+double sum(int a, double b, double c, double d) {
+  return a + b + c + d;
+}
+
+double sum(double a, int b, int c, int d) {
+  return a + b + c + d;
+}
+
+double sum(double a, int b, int c, double d) {
+  return a + b + c + d;
+}
+
+double sum(double a, int b, double c, int d) {
+  return a + b + c + d;
+}
+
+double sum(double a, int b, double c, double d) {
+  return a + b + c + d;
+}
+
+double sum(double a, double b, int c, int d) {
+  return a + b + c + d;
+}
+
+double sum(double a, double b, int c, double d) {
+  return a + b + c + d;
+}
+
+double sum(double a, double b, double c, int d) {
+  return a + b + c + d;
+}
+
+double sum(double a, double b, double c, double d) {
+  return a + b + c + d;
+}
+int main()
+{
+    cout<<sum(5,6)<<endl;
+    cout<<sum(5.9,6.8)<<endl;
+    cout<<sum(5.7,6)<<endl;
+    cout<<sum(5,6.3)<<endl;
+
+    cout<<sum(5,6,7)<<endl;
+    cout<<sum(5,6,7.5)<<endl;
+    cout<<sum(5,7.6,6)<<endl;
+    cout<<sum(5,6.8,3.7)<<endl;
+    cout<<sum(5.7,6,7)<<endl;
+    cout<<sum(5.7,6,7.8)<<endl;
+    cout<<sum(5.7,6.7,7)<<endl;
+    cout<<sum(5.7,6.7,7.8)<<endl;
+
+    cout<<sum(5,6,7,8)<<endl;
+    cout<<sum(5,6,7,8.8)<<endl;
+    cout<<sum(5,6,7.8,8)<<endl;
+    cout<<sum(5,6,7.5,8.8)<<endl;
+    cout<<sum(5,6.4,7,8)<<endl;
+    cout<<sum(5,6.7,7,8.8)<<endl;
+    cout<<sum(5,6.6,7.8,8)<<endl;
+    cout<<sum(5,6.6,7.8,8.9)<<endl;
+    cout<<sum(5.1,6,7,8)<<endl;
+    cout<<sum(5.1,6,7,8.7)<<endl;
+    cout<<sum(5.2,6,7.3,8)<<endl;
+    cout<<sum(5.7,6,7.7,6.9)<<endl;
+    cout<<sum(5.5,6.5,7,8)<<endl;
+    cout<<sum(5.4,6.3,7,8.8)<<endl;
+    cout<<sum(5.2,6.7,6.7,8)<<endl;
+    cout<<sum(5.5,6.4,7.8,8.9)<<endl;
+    }
+/*11
+12.7
+11.7
+11.3
+18
+18.5
+18.6
+15.5
+18.7
+19.5
+19.4
+20.2
+26
+26.8
+26.8
+27.3
+26.4
+27.5
+27.4
+28.3
+26.1
+26.8
+26.5
+26.3
+27
+27.5
+26.6
+28.6/*
